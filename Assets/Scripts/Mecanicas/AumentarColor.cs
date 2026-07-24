aee108d162f4f500e45b77afc588d439d590ec94
Theikogames <juanmanuelmirallesvega@gmail.com>
2026-07-24 14:10:58 -0400

    avance

--- Assets/Scripts/Mecanicas/AumentarColor.cs
+++ Assets/Scripts/Mecanicas/AumentarColor.cs
@@ -7,7 +7,7 @@ public class AumentarColor : MonoBehaviour
     public bool reclicar = false;
     public float gradienteColor = 0f;
     public float objetoGradienteColor = 0f;
-	public float velocidadPorSegundo = 1f;
+	public float velocidadPorSegundo = 5f;
 
 	private void OnTriggerEnter(Collider other)
     {
@@ -22,7 +22,7 @@ public class AumentarColor : MonoBehaviour
 	{
 		if (reclicar) 
         {
-            objetoGradienteColor += 3f;
+            objetoGradienteColor += 10f;
             reclicar = false;
         }
         gradienteColor = Mathf.MoveTowards(gradienteColor, objetoGradienteColor, velocidadPorSegundo * Time.deltaTime);
